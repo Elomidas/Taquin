d19be9db67f1f796cf509737940a142f6a1456e4
Elomidas <victor96@hotmail.fr>
2018-05-30 09:10:10 +0200

    Ajout du plateau

--- src/model/Agent.java
+++ src/model/Agent.java
@@ -3,9 +3,26 @@ package model;
 public class Agent {
     private Position position;
     private Position target;
+    private int id;
+
+    private static int _id = 0;
 
     public Agent(Position pos, Position targ) {
         position = pos;
         target = targ;
+        id = _id;
+        _id++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void Message(int targetId, String perform, String action, Position toFree) {
+        //TODO
     }
 }
--- src/model/Plateau.java
+++ src/model/Plateau.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Plateau {
+    private ArrayList<Agent> agents;
+    private int high, length;
+
+    public Plateau() {
+        this(5, 5);
+    }
+
+    public Plateau(int sizeX, int sizeY) {
+        agents = new ArrayList<>();
+        high = sizeY;
+        length = sizeX;
+    }
+
+    public Agent getAgent(int index) {
+        if(index < agents.size()) {
+            return agents.get(index);
+        }
+        return null;
+    }
+
+    public int size() {
+        return agents.size();
+    }
 }
