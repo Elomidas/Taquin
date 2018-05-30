3faf5243ccca97dc261529ba3f070ea170a14af1
bastienjacoud <bastien.jacoud@hotmail.fr>
2018-05-30 09:34:25 +0200

    Ajout des tests

---
+++ src/main/java/model/Position.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.Objects;
+
+public class Position {
+
+    private int px, py;
+
+    public Position(int x, int y) {
+        setPos(x, y);
+    }
+
+    public int getX() {
+        return px;
+    }
+
+    public int getY() {
+        return py;
+    }
+
+    public void setX(int x) {
+        px = x;
+    }
+
+    public void setY(int y) {
+        py = y;
+    }
+
+    public void setPos(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Position pos = (Position) obj;
+        if(pos.getX() != this.getX())
+            return false;
+        if(pos.getY() != this.getY())
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(px, py);
+    }
+}
